3e0a98e9594b0dde3b1f40a1274cb7c90fbd05f1
siddhant389 <siddhant389@gmail.com>
2026-06-08 14:19:18 -0700

    Add: permutation-string - submission-1

---
+++ Data Structures & Algorithms/permutation-string/submission-1.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String getKey(String str) {
+        int[] arr = new int[26];
+        for(char ch: str.toCharArray()){
+            arr[ch - 'a']++;
+        }
+        StringBuilder key = new StringBuilder();
+        for(int n : arr) {
+            key.append(n);
+        }
+
+        return key.toString();
+    }
+    public boolean checkInclusion(String s1, String s2) {
+        
+
+        String key = getKey(s1);
+        int i=0, j = s1.length()-1;
+
+        while(j < s2.length()) {
+            String window = s2.substring(i,j+1);
+            String windowKey = getKey(window);
+            if(windowKey.equals(key)) return true;
+            i++;
+            j++;
+        }
+        return false;
+    }
+}
